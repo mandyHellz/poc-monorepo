3e524fde79690d86a7c37016e9df8ed06a6fc519
Amanda Nunes <77729852+mandyHellz@users.noreply.github.com>
2022-03-22 10:22:06 -0300

    chore: added typescript to project for test purposes

--- packages/zika/subtract/index.js
+++
@@ -1,6 +0,0 @@
-function subtract(num1, num2) {
-    let total = num1 - num2
-    return total
-}
-
-module.exports = subtract
--- packages/zika/sum/index.js
+++
@@ -1,6 +0,0 @@
-function sum(num1, num2) {
-    let total = num1 + num2
-    return total
-}
-
-module.exports = sum
